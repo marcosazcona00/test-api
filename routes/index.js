696b3712ca509b12adee6a6cfbee1fc88a4f5740
jghm96 <goyihm@hotmail.com>
2024-11-12 22:43:58 -0300

    agrego ping

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,9 @@ router.get('/', function(req, res, next) {
   res.json({ status: 200, ok: true });
 });
 
+router.get('/ping', function(req, res, next) {
+  res.send('pong');
+});
+
 module.exports = router;
 
