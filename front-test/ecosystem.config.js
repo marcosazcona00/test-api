9c8846f19833b899edc8d2351760caf11314a770
jghm96 <goyihm@hotmail.com>
2024-11-13 23:31:17 -0300

    puerto del front

--- front-test/ecosystem.config.js
+++ front-test/ecosystem.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   apps : [{
     name   : "app2",
-    script : "nuxt dev"
+    name   : "app2",
+    script : "nuxt",
+    args   : "start", // Usa "start" en lugar de "dev" para producción
+    env: {
+      PORT: 3000 // Cambia este número al puerto que prefieras
+    }
   }]
 }
