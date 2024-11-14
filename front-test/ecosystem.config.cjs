cd7da5a99d4530716da01012c037d0e06b869826
jghm96 <goyihm@hotmail.com>
2024-11-13 23:39:29 -0300

    preubo gpt

--- front-test/ecosystem.config.cjs
+++ front-test/ecosystem.config.cjs
@@ -1,10 +1,10 @@
-export default {
+module.exports = {
   apps : [{
     name   : "app2",
     script : "nuxt",
-    args   : "start",
+    args   : "start", // Usa "start" en lugar de "dev" para producción
     env: {
       PORT: 3000 // Cambia este número al puerto que prefieras
     }
   }]
-};
+}
